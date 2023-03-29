f2473623a8414dbff40069460243a805aef4db6d
valerie_l <contacttovalerie@gmail.com>
2023-03-29 19:15:20 -0400

    Fixed typos

--- js/script.js
+++ js/script.js
@@ -2,27 +2,26 @@ var subTypeElement = document.querySelector("#subscription");
 var subDurationElement = document.querySelector("#months");
 var result = document.querySelector(".result");
 var subType = "basic";
-var subDuration = Number("1");
+var subDuration = 1;
 
 
-subTypeElement.addEventListener("change", function(e) {
-    subtype = e.target.value;
+subTypeElement.addEventListener("change", function (e) {
+    subType = e.target.value;
     updateSubscriptionDiv();
 });
 
-subDurationElement.addEventListener("change", function(e) {
+subDurationElement.addEventListener("change", function (e) {
     subDuration = Number(e.target.value);
     updateSubscriptionDiv();
 });
 
-var updateSubscriptionDiv = function (){
+var updateSubscriptionDiv = function () {
     var monthlyCost = 5;
     if (subType === "standard") {
         monthlyCost = 7;
-    } else if (subtype === "premium") {
+    } else if (subType === "premium") {
         monthlyCost = 10;
     }
-    
     var total = subDuration * monthlyCost;
 
     result.innerText = `You have chosen the ${subDuration}
